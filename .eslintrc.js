9e38ddf28779103be60009581dd881de8d985153
Felipe Pereira <felipe.pereira@loadsmart.com>
2021-03-21 18:08:39 -0300

    add husky

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'standard', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 80,
+				trailingComma: 'es5',
+				semi: false,
+				jsxSingleQuote: true,
+				singleQuote: true,
+				useTabs: true,
+			},
+		],
+	},
+}
--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export default styled.input`
 	width: 100%;
+
+	margin-bottom: 16px;
 	padding: 8px;
 
 	font-size: 14pt;
@@ -9,8 +11,6 @@ export default styled.input`
 	border: 1px solid #19535f;
 	border-radius: 8px;
 	outline: none;
-
-	margin-bottom: 16px;
 
 	&:focus {
 		padding: 7px;
